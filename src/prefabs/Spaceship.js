22fcf251aed14246c34a1d5f7b693fd77956aa09
ironleman <40055205+ironleman@users.noreply.github.com>
2020-04-13 18:51:40 -0700

    make movement correspond to difficulty

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -2,17 +2,21 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
-
+    
         scene.add.existing(this); // add object to existing scene, displayList, updateList
         this.points = pointValue;
     }
 
     update() {
         // move spaceship left
-        this.x -= 3;
+        this.x -= game.settings.spaceshipSpeed;
         // wraparound screen bounds
         if(this.x <= 0 - this.width) {
             this.x = game.config.width;
         }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
