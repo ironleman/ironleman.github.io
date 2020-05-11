31c48e114bb5edc2600d20b3968098dbeea7c79f
ironleman <40055205+ironleman@users.noreply.github.com>
2020-05-10 20:59:20 -0700

    Update Load.js

--- FinalProject/scenes/Load.js
+++ FinalProject/scenes/Load.js
@@ -1,1 +1,17 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super("loadScene");
+    }
 
+    preload() {
+        // set load path
+        this.load.path = "assets/";
+
+    }
+    create() {
+        // ...and pass to the next Scene
+
+        this.scene.start("menuScene");
+
+    } 
+}
