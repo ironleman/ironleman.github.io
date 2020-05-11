677b3394d98eb4084670e1a16603d9ccaadb89ca
ironleman <40055205+ironleman@users.noreply.github.com>
2020-05-11 12:34:47 -0700

    Update Load.js

--- FinalProject/scenes/Load.js
+++ FinalProject/scenes/Load.js
@@ -8,6 +8,10 @@ class Load extends Phaser.Scene {
         this.load.path = "assets/";
         this.load.image('pufferFish', 'pufferFish.png');
         this.load.image('tutorialBG', 'tutorialArtTemp.png');
+        this.load.image('key1', 'key1UI.png');
+        this.load.image('key2', 'key2UI.png');
+        this.load.image('key3', 'key3UI.png');
+
 
     }
     create() {
