b910bdad25e69a7b19ed32da5a942a758ed7fe7a
ironleman <40055205+ironleman@users.noreply.github.com>
2020-05-13 21:58:54 -0700

    Update Load.js

--- FinalProject/scenes/Load.js
+++ FinalProject/scenes/Load.js
@@ -8,14 +8,38 @@ class Load extends Phaser.Scene {
         this.load.path = "./assets/";
         //load all necessary assets
         this.load.image("corals", "Coral Reef.png");
-        this.load.image("pufferFish", "Player.png");
+        this.load.spritesheet("pufferFish", "Player.png", {
+            frameWidth: 417,
+            frameHeight: 261,
+            startFrame: 0,
+            endFrame: 0
+        });
+        this.load.spritesheet("pufferFat", "fatpuff.png", {
+            frameWidth: 640,
+            frameHeight: 640,
+            startFrame: 0,
+            endFrame: 0
+        });
+        this.load.spritesheet("pufferLong", "puffLong.png", {
+            frameWidth: 169,
+            frameHeight: 416,
+            startFrame: 0,
+            endFrame: 0
+        });
         this.load.video("oceanfloor", "Ocean Floor.mp4");
         this.load.image("play", "Play Button.png");
         this.load.image("select", "SelectionButton.png");
         this.load.image('tutorialBG', 'tutorialArtTemp.png');
-        this.load.image('key1', 'key1UI.png');
-        this.load.image('key2', 'key2UI.png');
-        this.load.image('key3', 'key3UI.png');
+        this.load.image('key2', 'key1UI.png');
+        this.load.image('key3', 'key2UI.png');
+        this.load.image('key4', 'key3UI.png');
+        this.load.image('key1', 'key0UI.png');
+        this.load.spritesheet('pufferS', 'pufferS.png',{
+            frameWidth: 420,
+            frameHeight: 126,
+            startFrame: 0,
+            endFrame: 0
+        });
         this.load.atlas("bubbles", "bubblesheet.png", "bubbles.json");
         this.load.audio("blip", "reverse_blip.mp3");
 
@@ -30,7 +54,7 @@ class Load extends Phaser.Scene {
         this.logo= this.add.text(480, centerY, "RDSJ L.L.C.", {fontFamily: "Bangers", fontSize: "80px", color: "#FF7F50"});
         this.logo.setAlpha(0.4);
         //create a time event that lasts 15 seconds before transitioning to the next scene
-        this.clock= this.time.delayedCall(15000, () => {
+        this.clock= this.time.delayedCall(1000, () => {
            
             this.scene.start("menuScene");
 
